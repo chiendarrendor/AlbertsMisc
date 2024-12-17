fae63b9d7fb7326b571080c8309282c3b175e7b5
Albert Baker <chiendarrendor@yahoo.com>
2024-12-17 02:37:50 -0800

    fixed bugs in !reading parsing and added coin-flip mechanic

--- PythonTaterBot/taterbot.py
+++ PythonTaterBot/taterbot.py
@@ -45,6 +45,14 @@ cardfile.close()
 
 logging.info('cards file read')
 
+logging.info('reading coin flip results')
+coinfile = open('coinflip.txt')
+coininfo = coinfile.read()
+coinfile.close()
+logging.info('coin flip results file read')
+
+
+
 documentname='Wayback Machine to the Crowdsource Tarot'
 documenturl='https://web.archive.org/web/20160310181245/http://crowdsourcetarot.makeincense.com/cards.phtml'
 
@@ -53,7 +61,9 @@ messageprefix = ""
 random.seed()
 
 # regex for 'reading'
-readparse = re.compile(r"!reading\s+((\d+)\s+)?.*") 
+readparse = re.compile(r"!reading(\s+.*)?")
+numparse = re.compile(r"\d+")
+coinparse = re.compile(r"!(coins|shells)(\s+.*)?")
 
 
 
@@ -75,13 +85,9 @@ def cardText(card):
 def doReading(count):
     readingcards = random.sample(cards['cards'],count)
     result = ""
-    for card in readingcards: 
+    for card in readingcards:
         result = result + cardText(card)
     return result
-    
-
-
-
 
 
 
@@ -95,19 +101,51 @@ class MyClient(discord.Client):
         if message.content == "!copyright":
             await message.channel.send(messageprefix + copyright)
             logging.info('user %s on channel %s requested copyright',message.author,message.channel)
-
+        elif message.content == "!coinhelp":
+            await message.channel.send(messageprefix + coininfo)
+            logging.info('user %s on channel %s requested coin help',message.author,message.channel)
         elif message.content == "!help":
             await message.channel.send(messageprefix + helptext)
             logging.info('user %s on channel %s requsted help',message.author,message.channel)
         elif message.content == "!link":
             await message.channel.send(messageprefix + "[" + documentname + "](" + documenturl + ")")
             logging.info('user %s on channel %s requested link to cards reference',message.author,message.channel)
-        
+
+        matcher = coinparse.match(message.content)
+        if matcher:
+            hcount = 0
+            for idx in range(4):
+                if random.randint(0,1) == 1:
+                    hcount = hcount + 1
+            coinmessage = ""
+            if hcount == 0:
+                coinmessage = "XXXX - NO. Stop divining on this topic now."
+            elif hcount == 1:
+                coinmessage = "OXXX - No.  You may ask more questions for clarity."
+            elif hcount == 2:
+                coinmessage = "OOXX - Yes."
+            elif hcount == 3:
+                coinmessage = "OOOX - First throw: Maybe, Ask again. Second throw: yes with conditions, ask more questions."
+            elif hcount == 4:
+                coinmessage = "OOOO - Yes!  With Blessings!"
+            else:
+                coinmessage = "???? - Wow!  Tell Albert about this, this should never happen"
+
+            logging.info('user %s on channel %s requested coinflip',message.author,message.channel)
+            await message.channel.send(messageprefix + coinmessage)
+
         matcher = readparse.match(message.content)
         if matcher:
+            # if we get here, we know that message.content starts with !reading, and if
+            # there are more characters in the test, there is whitespace after the reading
             cardcount = 3
-            if matcher.group(2):
-                cardcount = int(matcher.group(2))
+            wordlist = message.content.split()
+
+            # if the second word is a number, we want to use that as the wordcount
+            if len(wordlist) > 1:
+                nummatch = numparse.match(wordlist[1])
+                if nummatch:
+                    cardcount = int(wordlist[1])
 
             logging.info('user %s on channel %s requested reading on %s cards',message.author,message.channel,cardcount)
 
@@ -124,5 +162,3 @@ intents = discord.Intents.default()
 logging.info('starting bot server')
 client = MyClient(intents=intents)
 client.run(bot_token)
-
-
